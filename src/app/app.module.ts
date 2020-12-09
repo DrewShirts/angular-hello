0d5360eed8f5ebbf16175665d0a9ddeb22781e08
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-08 17:53:13 -0700

    pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { OrdersComponent } from './orders/orders.component';
 import { SearchBoxDirective } from './search-box.directive';
+import { InitCapsPipe } from './shared/init-caps.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
     OrdersComponent,
-    SearchBoxDirective
+    SearchBoxDirective,
+    InitCapsPipe
   ],
   imports: [
     BrowserModule,
