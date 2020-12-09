c22d1eb26d55af59f362fa7e5f55fc08c04a821c
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-08 17:42:35 -0700

    happy!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
